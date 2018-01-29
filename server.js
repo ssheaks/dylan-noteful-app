63b09fd559b211833749581aab0bf514b4727fce
Dylan <nielsen.dylan@gmail.com>
2018-01-29 11:53:38 -0800

    New server startup message

--- server.js
+++ server.js
@@ -3,14 +3,13 @@ const express = require('express');
 
 const data = require('./db/notes');
 
-console.log('hello world!');
+// console.log('hello world!');
 
 // INSERT EXPRESS APP CODE HERE...
 
 const app = express();
 
 app.use(express.static('public'));
-app.listen(8080);
 
 app.get('/v1/notes', (req, res) => {
   let {searchTerm} = req.query;
@@ -27,3 +26,11 @@ app.get('/v1/notes/:id', (req, res) => {
   const item = data.find(item => item.id === parseInt(req.params.id));
   res.json(item);
 });
+
+app
+  .listen(8080, function() {
+    console.info(`Server listening on ${this.address().port}`);
+  })
+  .on('error', err => {
+    console.error(err);
+  });
