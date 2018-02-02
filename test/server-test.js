2b1bd8a3d860d7081142535f5daa631a17d9f126
Dylan <nielsen.dylan@gmail.com>
2018-02-02 13:22:02 -0800

    Use chai spy in server test

--- test/server-test.js
+++ test/server-test.js
@@ -1,11 +1,13 @@
 'use strict';
 const app = require('../server');
 const chai = require('chai');
+const spy = require('chai-spies');
 const chaiHttp = require('chai-http');
 
 const expect = chai.expect;
 
 chai.use(chaiHttp);
+chai.use(spy);
 
 
 describe('Reality check', function() {
@@ -15,5 +17,34 @@ describe('Reality check', function() {
 
   it('2 + 2 should equal 4', function() {
     expect(2 + 2).to.equal(4);
+  });
+});
+
+describe('Express static', function() {
+  it('GET request "/" should return the index page', function() {
+    return chai
+      .request(app)
+      .get('/')
+      .then(function(res) {
+        expect(res).to.exist;
+        expect(res).to.have.status(200);
+        expect(res).to.be.html;
+      });
+  });
+});
+
+describe('404 handler', function() {
+  it('should respond with 404 when given a bad path', function() {
+    const spy = chai.spy();
+    return chai
+      .request(app)
+      .get('/bad/path')
+      .then(spy)
+      .then(() => {
+        expect(spy).to.not.have.been.called();
+      })
+      .catch(err => {
+        expect(err.response).to.have.status(404);
+      });
   });
 });
